f427c946e485d6ea67a89d2983908b1f7babf000
UmeshGolani <ssutg97@gmail.com>
2025-06-01 12:31:13 +0530

    update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
 const TodoList = ({ todos, handleComplete }) => {
-
-    return (
-        <ul>
-            {todos.map(todo =>(
-                <li key={todo.id}>
-                    {todo.text} {todo.complete ? true : ''}
-                    {!todo.complete && (
-                        <button onClick={() => handleComplete(todo.id)}>
-                            Complete
-                        </button>
-                    )}
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id} data-testid={`todo-${todo.id}`}>
+          {todo.text}
+          {todo.complete ? (
+            <span style={{ color: 'green' }}> (Completed)</span>
+          ) : (
+            <button onClick={() => handleComplete(todo.id)}>
+              Complete
+            </button>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
